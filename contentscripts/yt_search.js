7dcea945d524498c6074cabbe712eeca99d64065
Rick van de Loo <rickvandeloo@gmail.com>
2012-05-16 01:23:30 +0200

    Updates the video id filter for the youtube search page in yt_search.js

--- contentscripts/yt_search.js
+++ contentscripts/yt_search.js
@@ -8,13 +8,15 @@ var object_amount = (linkcount.length);
 for(i=0;i<object_amount;i++){
 	if(linkbar.children("h3")[i]){
 		search_object = linkbar.children("h3")[i].innerHTML;
-		youtube_id = search_object.split('"')[1];
+		youtube_id= search_object.split('href')[1];
+		youtube_id = youtube_id.split('"')[1];
 		youtube_id = youtube_id.split('v=')[1];
 		if (youtube_id){
 			var amprsnd = youtube_id.indexOf('&');
 			if(amprsnd != -1) {
 				youtube_id = youtube_id.substring(0, amprsnd);
 			}
+			console.log(youtube_id);
 			search_current = linkbar[i];
 			search_current.innerHTML += '<ul style="float: right;" class="single-line-lego-list"><li><a id="'+youtube_id+'" class="yt-badge-std playbutton" dir="ltr">play</a></li><li><a class="yt-badge-std plusbutton" id="'+youtube_id+'" dir="ltr">+</a></li></ul>'
 		}
